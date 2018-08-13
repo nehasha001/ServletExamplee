7317ca0e030a6154af18378922576f4d6df20eb6
nehasha001 <neha.shaik777@gmail.com>
2018-08-13 18:24:35 -0400

    initial commit

---
+++ out/artifacts/ServletExample_war_exploded/WEB-INF/Welcome.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Neha
+  Date: 8/12/2018
+  Time: 4:09 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<h1>
+    Welcome to server side programming with servlet.
+</h1>
+<body>
+
+</body>
+</html>
---
+++ src/WelcomeServlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet implements Servlet {
+    public void init(ServletConfig servletConfig) throws ServletException{
+        System.out.println("Inside ServletConfig");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        System.out.println("Inside getServletConfig");
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Inside Service");
+        PrintWriter printWriter = servletResponse.getWriter();
+        printWriter.print("<html><body>");
+        printWriter.print("<h1> Welcome to server side programming. </h1>");
+        printWriter.print("</body></html>");
+        ServletConfig config = getServletConfig();
+    }
+
+    @Override
+    public String getServletInfo() {
+        System.out.println("Inside getServletInfo");
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Inside destroy method");
+
+    }
+
+}
